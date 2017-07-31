ca86356d0802d6ab29d02d3cd491b60506e221d9
Georgi <geeorgyaja@gmail.com>
2017-07-31 15:34:49 +0300

    SignUp both company and Branch manager

--- WasteNoMore/app/components/signup/Signup1.js
+++ WasteNoMore/app/components/signup/Signup1.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Alert, StyleSheet, Text, View, TextInput, AppRegistry, Button, KeyboardAvoidingView  } from 'react-native';
+import {Alert, StyleSheet, Text, View, TextInput, AppRegistry, Button, KeyboardAvoidingView,AsyncStorage } from 'react-native';
 
 export default class Signup1 extends Component{
 
@@ -7,7 +7,17 @@ export default class Signup1 extends Component{
     super();
     this.state = {
       license: '',
-      email: ''
+      status:'',
+      role:'',
+      expiry:'',
+    }
+  }
+
+  async saveItem(item, selectedValue) {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.error('AsyncStorage error: ' + error.message);
     }
   }
 
@@ -16,10 +26,35 @@ export default class Signup1 extends Component{
    });
 
   _validateButtonPress = () => {
-    console.log(this.state.license);
-    console.log(this.state.email);
 
-    this.props.navigation.navigate('Signup2');
+  if(! this.state.license) return;
+
+
+
+  fetch('http://192.168.137.43:8000/api/license_authentication', {
+     method: 'POST',
+     headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+     body: JSON.stringify({
+       "license": 'b8ce1afd883f59422ad672bd42cdabb8',//this.state.license,
+       "timestamp": Date.now(),
+     })
+   })
+   .then((response) => response.json())
+   .then((responseData) => {
+     console.log(responseData);
+     if(responseData.status == "granted"){
+     //this.saveItem('license', responseData.license),
+     //this.saveItem('expiry', responseData.expiry),
+     this.saveItem('role', responseData.role),
+     this.saveItem('challange', responseData.random),
+     this.props.navigation.navigate('Signup2');
+   }else if (responseData.status == "denied"){
+     Alert.alert('Access Denied');
+   }else {
+     Alert.alert('Error');
+   }
+   })
+   .done();
   }
 
   render(){
@@ -27,20 +62,15 @@ export default class Signup1 extends Component{
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
         <Text style={styles.label}>License Key:</Text>
         <TextInput style={styles.input} placeholder="License Key"
-          onSubmitEditing={(event) => {
-            this.refs.SecondInput.focus();
-          }}
+          // onSubmitEditing={(event) => {
+          //   this.refs.SecondInput.focus();
+          // }}
           onChangeText={(text) => this.setState({license: text})}
-          returnKeyType = {"next"}
+
           value = {this.state.license}>
         </TextInput>
 
-        <Text style={styles.label}>Email:</Text>
-        <TextInput style={styles.input} placeholder="Email"
-          ref = 'SecondInput'
-          onChangeText={(text) => this.setState({email: text})}
-          value = {this.state.email}>
-        </TextInput>
+
 
         <View style={styles.buttonContainer}>
           <Button color="#841584" title="Validate" onPress={this._validateButtonPress}></Button>
--- WasteNoMore/app/components/signup/Signup2.js
+++ WasteNoMore/app/components/signup/Signup2.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Alert, StyleSheet, Text, View, TextInput, AppRegistry, Button, KeyboardAvoidingView  } from 'react-native';
+import {Alert, StyleSheet, Text, View, TextInput, AppRegistry, Button, KeyboardAvoidingView,AsyncStorage  } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 export default class Signup2 extends Component{
 
@@ -8,25 +8,102 @@ export default class Signup2 extends Component{
     this.state = {
       username: '',
       password: '',
+      email: '',
+      hasChallange: '',
       passwordConfirmation: '',
-      errorMessage: ''
+      errorMessage: '',
+      errorMessageUsername:'',
+      errorMessageEmail:''
     }
   }
 
 
+
+  async saveItem(item, selectedValue) {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.error('AsyncStorage error: ' + error.message);
+    }
+  }
+
+  clear_error_messages = () =>{
+    this.setState({errorMessage: '',
+  errorMessageEmail: '',
+errorMessageUsername: ''})
+  }
+
+  _validateEmail = (email) => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
 _validateSubmitPress = () => {
 
   if(this.state.password !== this.state.passwordConfirmation){
+    this.clear_error_messages();
     return this.setState({errorMessage: 'Passwords do not match'})
   }
 
-  this.props.navigation.dispatch(NavigationActions.reset(
-                 {
-                    index: 0,
-                    actions: [
-                      NavigationActions.navigate({ routeName: 'ManagerMain'},{user: 'Georgi'})
-                    ]
-                  }));
+  if (!this._validateEmail(this.state.email)){
+      this.clear_error_messages();
+    return this.setState({errorMessageEmail: "Please enter a valid email"})
+  }
+
+
+  AsyncStorage.getItem('challange').then((challange) => {
+     this.setState({ hasChallange: challange !== null });
+     if(this.state.hasChallange){
+
+       fetch('http://192.168.137.43:8000/api/signup', {
+          method: 'POST',
+          headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            "challange": challange,
+            "timestamp": Date.now(),
+            "username": this.state.username,
+            "password": this.state.password,
+            "email": this.state.email
+          })
+        })
+        .then((response) => response.json())
+        .then((responseData) => {
+          console.log(responseData);
+          //console.log("the status");
+        //  console.log(responseData.status);
+          if(responseData.status == "success"){
+          //this.saveItem('license', responseData.license),
+          //this.saveItem('expiry', responseData.expiry),
+          this.saveItem('token', responseData.token),
+          this.saveItem('username',this.state.username),
+
+            AsyncStorage.getItem('role').then((role) => {
+              if(role === "COMPANY_MANAGER"){
+                this.props.navigation.dispatch(NavigationActions.reset(
+                               {
+                                  index: 0,
+                                  actions: [
+                                    NavigationActions.navigate({ routeName: 'ManagerMain'})
+                                  ]
+                                }));
+            }else{
+                //ELSE go somewhere else
+                alert("User is Branch manager !");
+            }
+            })
+
+        }else if(responseData.status == "inputerror"){
+          this.state.errorMessageUsername = responseData.message.username;
+          this.state.errorMessageEmail = responseData.message.email;
+        }else {
+          Alert.alert('Error');
+        }
+        })
+        .done();
+     }
+   })
+
+
 }
 
   static navigationOptions = ({ navigation }) => ({
@@ -46,13 +123,27 @@ _validateSubmitPress = () => {
            value = {this.state.username}
            onChangeText={(text) => this.setState({username: text})}>
          </TextInput>
+         <Text style={styles.errorLabel}>{this.state.errorMessageUsername}</Text>
+
+         <Text style={styles.label}>Email:</Text>
+         <TextInput style={styles.input} placeholder="Email"
+           ref='SecondInput'
+           returnKeyType = {"next"}
+           onChangeText={(text) => this.setState({email: text})}
+           value = {this.state.email}
+           onSubmitEditing={(event) => {
+             this.refs.ThirdInput.focus();
+           }}>
+
+         </TextInput>
+         <Text style={styles.errorLabel}>{this.state.errorMessageEmail}</Text>
 
          <Text style={styles.label}>Password:</Text>
          <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}
-           ref='SecondInput'
+           ref='ThirdInput'
            returnKeyType = {"next"}
            onSubmitEditing={(event) => {
-             this.refs.ThirdInput.focus();
+             this.refs.ForthInput.focus();
            }}
            value = {this.state.password}
            onChangeText={(text) => this.setState({password: text})}>
@@ -60,14 +151,13 @@ _validateSubmitPress = () => {
 
          <Text style={styles.label}>Repeat Password:</Text>
          <TextInput style={styles.input} placeholder="Repeat Password" secureTextEntry={true}
-           ref='ThirdInput'
+           ref='ForthInput'
            value = {this.state.passwordConfirmation}
            onChangeText={(text) => this.setState({passwordConfirmation: text})}>
          </TextInput>
 
 
          <Text style={styles.errorLabel}>{this.state.errorMessage}</Text>
-
 
          <View style={styles.buttonContainer}>
            <Button color="#841584" title="Submit" onPress={this._validateSubmitPress}></Button>
