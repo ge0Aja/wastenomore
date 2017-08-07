031597325942b7d9709d31ac970048e21a4159d4
Georgi <geeorgyaja@gmail.com>
2017-08-07 20:30:24 +0300

    branch management not complete

--- WasteNoMore/app/components/companyManager/AddBranch.js
+++ WasteNoMore/app/components/companyManager/AddBranch.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View,AppRegistry, TouchableOpacity,ActivityIndicator,Picker,Alert, Modal, Dimensions, ListView, TextInput,Keyboard,TouchableHighlight} from 'react-native';
+import { StyleSheet, Text, View,AppRegistry, TouchableOpacity,ActivityIndicator,Picker,Alert, Modal, Dimensions, ListView, TextInput,Keyboard,TouchableHighlight, Switch} from 'react-native';
 
 import demoData from './demoBranchData.js';
 import DateTimePicker from 'react-native-modal-datetime-picker';
@@ -10,30 +10,47 @@ export default class AddBranch extends Component {
 
   constructor(){
     super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
     var {height, width} = Dimensions.get('window');
     this.state = {
       branchLocation:'',
       branchStaffCount:'',
       branchOpeningDate:'',
       branches:[],
+      gotBranches:false,
       branchesExist:false,
       modalVisible: false,
       modalBackgroundWidth:width,
       modalBackgroundHeight:height,
       locationList:[],
+      gotLocations:false,
       isDateTimePickerVisible: false,
       branchOpeningDateBeauty:'',
       modal2Visible: false,
-      dataSource: ds.cloneWithRows(demoData),
+      branchData:[],
+      dataSource:[],
       branchLocationEdit:'',
       branchStaffCountEdit:'',
       branchOpeningDateEdit:'',
-      branchOpeningDateEditBeauty:''
-
+      branchOpeningDateEditBeauty:'',
+      mainBranchAdd:false,
+      mainBranchEdit:false
     }
+      this.getBranches = this.getBranches.bind(this);
+      this.getLocations = this.getLocations.bind(this);
+  }
 
-  //  console.log(this.state.dataSource);
+  componentDidMount() {
+    this.getBranches();
+    this.getLocations();
+  }
+
+  _setMainBranch(value){
+    this.setState({mainBranchAdd:value})
+  }
+
+  _setMainBranchEdit(value){
+    this.setState({mainBranchEdit:value})
   }
 
   setModalVisible(visible) {
@@ -92,15 +109,19 @@ export default class AddBranch extends Component {
     alert("Branch should be edited");
   }
 
+  handelBranchDelete(){
+    alert("Branch will be deleted");
+  }
+
 
   _renderRow(rowData: Object, sectionID: number, rowID: number) {
      return (
        <View style={{padding:5}}>
          <TouchableHighlight underlayColor='rgba(211,211,211,0.9)' activeOpacity={3} onPress={this._onPressRow.bind(rowID, rowData)}>
            <View >
-             <Text style={styles.listTextLarge}>{rowData.location}</Text>
+             <Text style={styles.listTextLarge}>{rowData.location}/{rowData.location_district}/{rowData.location_governorate}</Text>
              <Text style={styles.listTextSmall}>Staff Count: {rowData.staff_count}</Text>
-             <Text style={styles.listTextSmall}>Opening Date: {rowData.opening_date}</Text>
+             <Text style={styles.listTextSmall}>Opening Date: {Moment(rowData.opening_date.date).format('MMM-DD-YYYY')}</Text>
            </View>
          </TouchableHighlight>
        </View>
@@ -110,12 +131,68 @@ export default class AddBranch extends Component {
   _onPressRow = (rowID, rowData) => {
   //  console.log(rowID);
     this.setState({
-      branchLocationEdit:rowID.location,
-      branchStaffCountEdit:rowID.staff_count,
+      branchLocationEdit:rowID.location+"/"+rowID.location_district,
+      branchStaffCountEdit:String(rowID.staff_count),
+      mainBranchEdit: (rowID.mainBranch == "true")? true : false
     });
-    this.setState({branchOpeningDateEditBeauty: Moment(rowID.opening_date).format('MMM-DD-YYYY')});
+    this.setState({branchOpeningDateEditBeauty: Moment(rowID.opening_date.date).format('MMM-DD-YYYY')});
     this.setModal2Visible(true);
   };
+
+  getBranches(){
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    //var TOKEN = await AsyncStorage.getItem('token');
+    fetch('http://192.168.137.43:8000/api/getCompanyBranches', {
+      headers: {
+        'Accept': 'application/json',
+        //  'Authorization': 'Bearer ' + TOKEN
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJnZW9hamEiLCJpYXQiOjE1MDIxMjYwOTQsImV4cCI6MTUwMjEyOTY5NH0.Kfs4yy_4j7nsVog48oJ9BCBkWLF_Cssy5cYrEFVgSDMNuY0cEqTvi29Y5M3EoHeHin9iHX4YvuYbOVLAbGCN-82GfQHZQXzgUitiJN4V6MmoGW5TPyO8zVvxCtm4GieGxh03Or2vxlvMcrY6aC6wzVKg9iXIANu7wMz03DEtXjydqm9GTEYEZ3MS5BKma9xUPM7rfi9ub0h5y_MgyIUtKHojNgbvL0TLJeyEQCA5R0_UbgMR_2-JtS2n6yH6Q17Jk7hG_ijgQN0oyC2euo8SGhbU5JO6-zqRCZk33zVSQJLRrh-NHphpc82i6ijqckg_VUMYG4OgXR5oFLwCfjt8mR7mWdRx3QKij-_TmDQwIzhP1vLqfV3GiEPdx1cK3neKtUOMHuITkU3NSjHzlNBYLZSHzcMA6k74S5c28MqmWsBTYIOJSnyE6O_LsuWnf1AEMjF2s3kgSIlpwxbqb9djkSgkBBWsZ1VV8lj2DGu7KgSngsOXutLbf2q0jYPwHWkV055FA_xkdfmqCcdXC8TTMKlm40Qs2vRV4WnaA0sIdcbRV0d7TVsNVe5NLT6-QKB4HmTqaSVUbxkX4h7UdOslFU2Yr7CsYrwlDUJ6WG51QFIW7B1y5ZPJGNJdfvMt3sLuJbmpodL7yw08MBrFiM5HOxkdUXc0VJbX1DnDSt4-b_c'
+      }
+    })
+        .then((response) => response.json())
+        .then((responseData) => {
+          if("message" in responseData){
+                console.log(responseData.message);
+          }
+          if(responseData.status == "error"){
+              console.log("error");
+          }else if(responseData.status == "success"){
+            this.setState({ branchData:responseData.branches,dataSource: ds.cloneWithRows(responseData.branches), gotBranches:true}); //isLoading: false,
+          }
+        })
+        .done();
+  }
+
+  getLocations(){
+    //var TOKEN = await AsyncStorage.getItem('token');
+    fetch('http://192.168.137.43:8000/api/getLocations', {
+      headers: {
+        'Accept': 'application/json',
+        //  'Authorization': 'Bearer ' + TOKEN
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJnZW9hamEiLCJpYXQiOjE1MDIxMjYwOTQsImV4cCI6MTUwMjEyOTY5NH0.Kfs4yy_4j7nsVog48oJ9BCBkWLF_Cssy5cYrEFVgSDMNuY0cEqTvi29Y5M3EoHeHin9iHX4YvuYbOVLAbGCN-82GfQHZQXzgUitiJN4V6MmoGW5TPyO8zVvxCtm4GieGxh03Or2vxlvMcrY6aC6wzVKg9iXIANu7wMz03DEtXjydqm9GTEYEZ3MS5BKma9xUPM7rfi9ub0h5y_MgyIUtKHojNgbvL0TLJeyEQCA5R0_UbgMR_2-JtS2n6yH6Q17Jk7hG_ijgQN0oyC2euo8SGhbU5JO6-zqRCZk33zVSQJLRrh-NHphpc82i6ijqckg_VUMYG4OgXR5oFLwCfjt8mR7mWdRx3QKij-_TmDQwIzhP1vLqfV3GiEPdx1cK3neKtUOMHuITkU3NSjHzlNBYLZSHzcMA6k74S5c28MqmWsBTYIOJSnyE6O_LsuWnf1AEMjF2s3kgSIlpwxbqb9djkSgkBBWsZ1VV8lj2DGu7KgSngsOXutLbf2q0jYPwHWkV055FA_xkdfmqCcdXC8TTMKlm40Qs2vRV4WnaA0sIdcbRV0d7TVsNVe5NLT6-QKB4HmTqaSVUbxkX4h7UdOslFU2Yr7CsYrwlDUJ6WG51QFIW7B1y5ZPJGNJdfvMt3sLuJbmpodL7yw08MBrFiM5HOxkdUXc0VJbX1DnDSt4-b_c'
+      }
+    })
+        .then((response) => response.json())
+        .then((responseData) => {
+        //  console.log(responseData.location);
+          if("message" in responseData){
+                console.log(responseData.message);
+          }
+          if(responseData.status == "error"){
+              console.log("error");
+          }else if(responseData.status == "success"){
+            this.setState({ locationList:responseData.location, gotLocations:true}); //isLoading: false,
+
+            this.state.locationList.map((s, i) => {
+              console.log(s);
+            });
+          }
+        })
+        .done();
+
+
+  }
 
   static navigationOptions = {
      drawerLabel: 'Add Branch',
@@ -128,7 +205,7 @@ export default class AddBranch extends Component {
    };
 
   render(){
-
+    if(this.state.gotBranches && this.state.gotLocations){
     return(
       <View style={styles.container}>
         <Modal
@@ -137,7 +214,6 @@ export default class AddBranch extends Component {
           visible={this.state.modalVisible}
           onRequestClose={() => {this._resetModal()}}
         >
-
           <View style={[styles.darkBackgroundModal, {width:this.state.modalBackgroundWidth, height:this.state.modalBackgroundHeight}]}>
 
             <View style={styles.lightBackgroundModal}>
@@ -147,9 +223,12 @@ export default class AddBranch extends Component {
                   selectedValue={this.state.branchLocation}
                   onValueChange={(itemValue) => this.setState({branchLocation: itemValue})}
                 >
-                  {this.state.locationList.map( (s, i) => {
-                    return <Picker.Item key={i} value={s} label={s} />
-                  })}
+                  <Picker.Item key={0} value={"0"} label={"Choose Location"}  />
+                  {
+                    this.state.locationList.map( (s, i) => {
+                      return <Picker.Item key={i} value={s.city+"/"+s.district} label={s.city+"/"+s.district} />
+                    })
+                  }
                 </Picker>
               </View>
               <View style={styles.modalLines}>
@@ -187,7 +266,14 @@ export default class AddBranch extends Component {
                 onCancel={this._hideDateTimePicker}
                 mode="date"
               />
+              <View style={styles.modalLines}>
+                <Text>
+                  Main Branch
+                </Text>
+                <Switch value={this.state.mainBranchAdd} onValueChange={(value) => {this._setMainBranch(value);}}>
 
+                </Switch>
+              </View>
               <TouchableOpacity style={styles.addBranchInner} onPress={this.handelBranchAdd}>
                 <Text style={{color:'darkviolet'}}>
                   Add Branch
@@ -214,9 +300,11 @@ export default class AddBranch extends Component {
                   selectedValue={this.state.branchLocationEdit}
                   onValueChange={(itemValue) => this.setState({branchLocationEdit: itemValue})}
                 >
-                  {this.state.locationList.map( (s, i) => {
-                    return <Picker.Item key={i} value={s} label={s} />
-                  })}
+                  {
+                    this.state.locationList.map( (s, i) => {
+                      return <Picker.Item key={i} value={s.city+"/"+s.district} label={s.city+"/"+s.district} />
+                    })
+                  }
                 </Picker>
               </View>
               <View style={styles.modalLines}>
@@ -255,11 +343,27 @@ export default class AddBranch extends Component {
                 mode="date"
               />
 
+              <View style={styles.modalLines}>
+                <Text >
+                  Main Branch
+                </Text>
+                <Switch value={this.state.mainBranchEdit} onValueChange={(value) => {this._setMainBranchEdit(value);}}>
+
+                </Switch>
+              </View>
+
               <TouchableOpacity style={styles.addBranchInner} onPress={this.handelBranchEdit}>
                 <Text style={{color:'darkviolet'}}>
                   Apply
                 </Text>
               </TouchableOpacity>
+              <View style={{marginTop:20}}>
+                <TouchableOpacity style={styles.delBranchInner} onPress={this.handelBranchDelete}>
+                  <Text style={{color:'red'}}>
+                    Delete Branch
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </Modal>
@@ -278,33 +382,28 @@ export default class AddBranch extends Component {
           </TouchableOpacity>
         </View>
       </View>
+    );}
+    else{
+      return(
+      <View style={styles.container}>
+        <ActivityIndicator
+          animating={!this.state.attrExist}
+          size={"large"}
+          hidesWhenStopped={true}
+        >
+        </ActivityIndicator>
+
+      </View>
     );
+    }
   }
 }
 
-
-
-// const Row = (props) => (
-//   <View style={{padding:5}}>
-//     <TouchableHighlight onPress={() => {
-//       this._pressRow(props)
-//     }}>
-//       <View >
-//         <Text style={styles.listTextLarge}>{props.location}</Text>
-//         <Text style={styles.listTextSmall}>Staff Count: {props.staff_count}</Text>
-//         <Text style={styles.listTextSmall}>Opening Date: {props.opening_date}</Text>
-//       </View>
-//     </TouchableHighlight>
-//   </View>
-// );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  // touchRow:{
-  //   backgroundColor: 'rgba(211,211,211,0.65)'
-  // },
   separator: {
     flex: 1,
     height: StyleSheet.hairlineWidth,
@@ -324,6 +423,15 @@ const styles = StyleSheet.create({
   },
   addBranchInner:{
     borderColor: 'darkviolet',
+    borderWidth: StyleSheet.hairlineWidth,
+    height: 30,
+    width: 100,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  delBranchInner:{
+    borderColor: 'red',
     borderWidth: StyleSheet.hairlineWidth,
     height: 30,
     width: 100,
